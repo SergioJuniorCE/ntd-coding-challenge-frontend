b962268b0ccb2d734b3e3683a5b92a18eeefc041
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2023-05-02 16:33:40 -0700

    updated vite config

--- src/lib/services/UserService.ts
+++ src/lib/services/UserService.ts
@@ -17,26 +17,7 @@ export class UserService extends BaseService {
   }
 
   public async logout() {
-    function getCookie(name: string) {
-      let cookieValue = null;
-      if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i].trim();
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === (name + '=')) {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-            break;
-          }
-        }
-      }
-      return cookieValue;
-    }
-    const response = await this.axios.post(`${this.baseUrl}/logout`, {
-      headers: {
-        "X-CSRFToken": getCookie('csrftoken')
-      }
-    });
+    const response = await this.axios.post(`${this.baseUrl}/logout`);
     return response.data;
   }
 
